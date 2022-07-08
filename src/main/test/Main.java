456516acf6d349629cadad10fc75e7f4390f678d
Taewoongjung <aipooh8882@naver.com>
2022-07-08 14:50:40 +0900

    Add 3085

--- src/main/test/Main.java
+++ src/main/test/Main.java
@@ -1,30 +1,21 @@
 package main.test;
 
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-//        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); //선언
-//        String s = bf.readLine(); //String
-//        int i = Integer.parseInt(bf.readLine()); //Int
-//
-//        StringTokenizer st = new StringTokenizer(s); //StringTokenizer인자값에 입력 문자열 넣음
-//        int a = Integer.parseInt(st.nextToken()); //첫번째 호출
-//        int b = Integer.parseInt(st.nextToken()); //두번째 호출
-//
-//        String array[] = s.split(" "); //공백마다 데이터 끊어서 배열에 넣음
-//
-//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));   //할당된 버퍼에 값 넣어주기
-//        String zz = "abcdefg";   //출력할 문자열
-//        bw.write(zz+"\n");   //버퍼에 있는 값 전부 출력
-//        bw.flush();   //남아있는 데이터를 모두 출력시킴
-//        bw.close();   //스트림을 닫음
-        int a = 1;
-        int b = 2;
-        int c = 3;
+        System.out.println("main class started");
+        Calculator calc = new Calculator();
+        int a = 10;
+        int b = 5;
+        int result1 = calc.add(a, b);
+        int result2 = calc.substract(a, b);
+        int result3 = calc.multiply(a, b);
+        int result4 = calc.divide(a, b);
 
-        a+= b+c;
-        System.out.println(a);
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
     }
 }
