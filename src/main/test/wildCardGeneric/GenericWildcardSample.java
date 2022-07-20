94bc2bf36fb3df4ca1eb04f55b4813a2e06d8a55
Taewoongjung <aipooh8882@naver.com>
2022-07-21 01:47:37 +0900

    Add 15651 and re_solve

---
+++ src/main/test/wildCardGeneric/GenericWildcardSample.java
@@ -0,0 +1,26 @@
+package main.test.wildCardGeneric;
+
+import main.test.generic.WildCardGeneric;
+
+public class GenericWildcardSample {
+    public static void main(String[] args) {
+        GenericWildcardSample genericWildcardSample = new GenericWildcardSample();
+        genericWildcardSample.callGenericMethod();
+    }
+
+//    public <T> void genericMethod(WildCardGeneric<T> c, T addValue) {
+    public <T, S> void genericMethod(WildCardGeneric<T> c, T addValue, S another) {
+        c.setWildcard(addValue);
+        System.out.println(another);
+        T value = c.getWildcard();
+        System.out.println(value);
+    }
+
+    public void callGenericMethod() {
+//        WildCardGeneric<String> wildcard = new WildCardGeneric<String>();
+        WildCardGeneric<Integer> wildcard = new WildCardGeneric<Integer>();
+
+//        genericMethod(wildcard, "Data");
+        genericMethod(wildcard, 1, 2);
+    }
+}
