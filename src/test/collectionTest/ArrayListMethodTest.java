9312ec713e7684f096f847ed5e30e10095f7cc30
Taewoongjung <aipooh8882@naver.com>
2022-08-13 04:49:20 +0900

    Add String Map Set test

--- src/test/collectionTest/ArrayListMethodTest.java
+++ src/test/collectionTest/ArrayListMethodTest.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ArrayListMethodTest {
+
     public static void main(String[] args) {
         ArrayListMethodTest arrayListMethodTest = new ArrayListMethodTest();
         List list = new ArrayList<>();
