027d39ed156f925c75cccce289a08ad15de6ad29
Taewoongjung <aipooh8882@naver.com>
2022-09-22 02:01:46 +0900

    Git action test

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,10 +1,40 @@
 package test;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Calculator cal = null;
-        System.out.println("@ = "+ String.valueOf(cal));
+//        String a = new String("A");
+//        String b = new String("A");
+//        System.out.println(a.equals(b));
+
+        List<Object> arr = new ArrayList<Object>();
+        arr.add(1);
+        arr.add("1");
+
+//        List<?> arr1 = new ArrayList<>();
+//        arr1.add(1);
+//        arr1.add("1");
+
+
+        List<?> arr11 = getList();
+        System.out.println(arr11);
+    }
+
+    public static List<Object> getList() {
+        List<Object> arr = new ArrayList<Object>();
+        arr.add(1);
+        arr.add("1");
+        return arr;
+    }
+
+    public static List<Object> getList() {
+        List<Object> arr = new ArrayList<Object>();
+        arr.add(1);
+        arr.add("1");
+        return arr;
     }
 }
---
+++ src/test/thread/practice/TimerMain.java
@@ -0,0 +1,9 @@
+package test.thread.practice;
+
+public class TimerMain {
+    public static void main(String[] args) {
+        TimerThread thread = new TimerThread();
+        thread.start();
+    }
+
+}
