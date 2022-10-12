f49d20f1b305013bce6c91721afcd5056af6d7dc
Taewoongjung <aipooh8882@naver.com>
2022-10-12 17:27:03 +0900

    7576 resolve

---
+++ src/re_solve/boj_7576/Main.java
@@ -0,0 +1,90 @@
+package re_solve.boj_7576;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int M;
+    static int[][] arr;
+    static int[][] dist;
+    static boolean[][] visited;
+    static int[] dir_x = {0, 0, 1, -1};
+    static int[] dir_y = {1, -1, 0, 0};
+    static int max = Integer.MIN_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+
+        arr = new int[N][M];
+        visited = new boolean[N][M];
+
+        Queue<Pair> q = new LinkedList<>();
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                if (arr[i][j] == 1) {
+                    q.offer(new Pair(i, j));
+                }
+            }
+        }
+
+        while (!q.isEmpty()) {
+            Pair cur = q.poll();
+            int cur_x = cur.getFirst();
+            int cur_y = cur.getSecond();
+
+            for (int i = 0; i < 4; i++) {
+                int next_x = dir_x[i] + cur_x;
+                int next_y = dir_y[i] + cur_y;
+
+                if (next_x < 0 || next_x >= N || next_y < 0 || next_y >= M) continue;
+                if(arr[next_x][next_y] !=0) continue;
+
+                arr[next_x][next_y] = arr[cur_x][cur_y] + 1;
+                q.offer(new Pair(next_x, next_y));
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                System.out.print(arr[i][j] + " ");
+                if (arr[i][j] == 0) {
+                    System.out.println("-1");
+                    System.exit(0);
+                }
+                max = Math.max(max, arr[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println(max - 1);
+
+    }
+}
+
+class Pair {
+    private final int x;
+    private final int y;
+
+    public Pair(final int x, final int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getFirst() {
+        return this.x;
+    }
+
+    public int getSecond() {
+        return this.y;
+    }
+}
