ff8703989d52e57dbfb0f7db9bc228d2ecd48e8e
Taewoongjung <aipooh8882@naver.com>
2022-11-12 01:32:42 +0900

    8958

---
+++ src/bronze/boj_8958/Main.java
@@ -0,0 +1,38 @@
+package bronze.boj_8958;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            list.add(br.readLine());
+        }
+
+        for (int i = 0; i < N; i++) {
+            String tmp = list.get(i);
+            int cnt = 0;
+            int tmp_cnt = 0;
+            for (int j = 0; j < tmp.length(); j++) {
+                char OorX = tmp.charAt(j);
+                if (OorX == 'O') {
+                    cnt++;
+                    tmp_cnt += cnt;
+                }
+                if(OorX == 'X') {
+                    cnt = 0;
+                }
+            }
+            System.out.println(tmp_cnt);
+        }
+    }
+}
