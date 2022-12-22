42c68c3fbc4377d4f370cbd706f88a61ccde72f2
Taewoongjung <aipooh8882@naver.com>
2022-12-23 01:13:23 +0900

    boj 2789

--- src/tier/bronze/boj_2789/Main.java
+++ src/tier/bronze/boj_2789/Main.java
@@ -1,5 +1,41 @@
 package tier.bronze.boj_2789;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] nums = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int result = 0;
+        int sum = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                for (int k = j + 1; k < N; k++) {
+
+                    sum = nums[i] + nums[j] + nums[k];
+
+                    if (result < sum && sum <= M) {
+                        result = sum;
+                    }
+                }
+            }
+        }
+        System.out.println(result);
+    }
 }
